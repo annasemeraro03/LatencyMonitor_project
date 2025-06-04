17fad3204dd4a876cdcddae2fe63d14e0b7670d4
Anna Semeraro <317383@studenti.unimore.it>
2025-06-04 22:30:37 +0200

    updated setup.sh to regenerate Pipfile.lock

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Crea il Pipfile e installa Django
-pipenv install django
+[ -f Pipfile.lock ] && rm Pipfile.lock
 
-# Attiva la shell dell'ambiente virtuale
+# create the Pipfile and install Django
+pipenv install django==4.2.22 || exit 1
+
+# activate shell of virtual environment
 exec pipenv shell
+
+# create a new Django project
+pipenv run python manage.py runserver
