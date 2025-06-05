cc84b65d2fb9708459c37c55f1eb6506a3fbb832
Anna Semeraro <317383@studenti.unimore.it>
2025-06-05 11:59:26 +0200

    upgraded setup.sh to install other libraries and update comments

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# check if Pipfile.lock exists and remove it if it does
 [ -f Pipfile.lock ] && rm Pipfile.lock
 
-# create the Pipfile and install Django
+# create the Pipfile and install libraries
 pipenv install django==4.2.22 || exit 1
+pipenv install djangorestframework || exit 1
+pipenv install pillow || exit 1
+pipenv install plotly || exit 1
+pipenv install weasyprint || exit 1
 
 # activate shell of virtual environment
 exec pipenv shell
-
-# create a new Django project
-pipenv run python manage.py runserver
