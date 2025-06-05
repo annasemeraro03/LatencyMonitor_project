05412efbeedb5d9b61f572b5904fc57d6fcb2eac
Anna Semeraro <317383@studenti.unimore.it>
2025-06-05 11:58:12 +0200

    created activate_server.sh script

---
+++ activate_server.sh
@@ -0,0 +1,6 @@
+# create a new Django project
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py createsuperuser
+
+python manage.py runserver localhost:8000 || exit 1
