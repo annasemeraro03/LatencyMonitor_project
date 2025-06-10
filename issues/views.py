b21eaac70ed52f2c311aaf53411eab27019988cb
Anna Semeraro <317383@studenti.unimore.it>
2025-06-11 00:46:40 +0200

    modified views.py for issues app

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_issues(request):
+    # Qui metti la logica per mostrare la lista degli esperimenti
+    return render(request, 'issues/list.html')
