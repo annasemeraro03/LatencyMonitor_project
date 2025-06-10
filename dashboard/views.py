50ded6156ea442ef89477a0f8493e14255cae1ca
Anna Semeraro <317383@studenti.unimore.it>
2025-06-11 00:44:09 +0200

    created views.py for dashboard app

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def view(request):
+    # Qui metti la logica per mostrare la lista degli esperimenti
+    return render(request, 'dashboard/view.html')
