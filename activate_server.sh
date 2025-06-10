0454214cfb657cd6d097a3dd006636be8feef073
Anna Semeraro <317383@studenti.unimore.it>
2025-06-11 00:36:57 +0200

    modified scripts activate_server.sh and setup.sh
    created file requirements.txt used in setup.sh

--- activate_server.sh
+++ activate_server.sh
@@ -1,6 +1,15 @@
-# create a new Django project
+# makemigrations 
+python manage.py makemigrations users
+python manage.py makemigrations issues
+python manage.py makemigrations experiments
+python manage.py makemigrations dashboard
+python manage.py makemigrations core
+
+python manage.py migrate
+
 # python manage.py makemigrations
 # python manage.py migrate
 # python manage.py createsuperuser
 
+#run server
 python manage.py runserver localhost:8000 || exit 1
--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@
 [ -f Pipfile.lock ] && rm Pipfile.lock
 
 # create the Pipfile and install libraries
-pipenv install django==4.2.22 || exit 1
-pipenv install djangorestframework || exit 1
-pipenv install pillow || exit 1
-pipenv install plotly || exit 1
-pipenv install weasyprint || exit 1
+pipenv install -r requirements.txt || exit 1
+
+# pipenv install django==4.2.22 || exit 1
+# pipenv install djangorestframework || exit 1
+# pipenv install pillow || exit 1
+# pipenv install plotly || exit 1
+# pipenv install weasyprint || exit 1
 
 # activate shell of virtual environment
 exec pipenv shell
