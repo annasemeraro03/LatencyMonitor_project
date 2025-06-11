614ffbe02a731355b44290b8893b5195d44da2c1
Anna Semeraro <317383@studenti.unimore.it>
2025-06-11 20:42:14 +0200

    modified script to add migrate and superuser

--- activate_server.sh
+++ activate_server.sh
@@ -5,11 +5,12 @@ python manage.py makemigrations experiments
 python manage.py makemigrations dashboard
 python manage.py makemigrations core
 
+# migrate
 python manage.py migrate
 
-# python manage.py makemigrations
-# python manage.py migrate
+# create superuser if needed
 # python manage.py createsuperuser
+# user: admin, password: admin
 
 #run server
 python manage.py runserver localhost:8000 || exit 1
--- setup.sh
+++ setup.sh
@@ -6,11 +6,5 @@
 # create the Pipfile and install libraries
 pipenv install -r requirements.txt || exit 1
 
-# pipenv install django==4.2.22 || exit 1
-# pipenv install djangorestframework || exit 1
-# pipenv install pillow || exit 1
-# pipenv install plotly || exit 1
-# pipenv install weasyprint || exit 1
-
 # activate shell of virtual environment
 exec pipenv shell
