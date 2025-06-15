a661680461875d8ebe7fe43feb39d4c008767b42
annasemeraro <317383@studenti.unimore.it>
2025-06-16 01:49:41 +0200

    added comment

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Rimuove lock precedente
+# Revove precedent lock
 [ -f Pipfile.lock ] && rm Pipfile.lock
 
-# Installa i pacchetti Python dal requirements.txt
+# Install Python packets from requirements.txt
 pipenv install -r requirements.txt || exit 1
 
-# Installa le dipendenze di sistema (non Python) per WeasyPrint
+# Install system's dependencies (not Python) for WeasyPrint
 sudo apt update || exit 1
 sudo apt install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info fonts-liberation || exit 1
 
-# Attiva l'ambiente virtuale
+# Activate virtual environment
 exec pipenv shell
