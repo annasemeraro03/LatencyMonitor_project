ac82ddebd9aef56001afb54c2c6224526180dcb1
Anna Semeraro <317383@studenti.unimore.it>
2025-06-15 12:16:19 +0200

    added report pdf logic

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# check if Pipfile.lock exists and remove it if it does
+# Rimuove lock precedente
 [ -f Pipfile.lock ] && rm Pipfile.lock
 
-# create the Pipfile and install libraries
+# Installa i pacchetti Python dal requirements.txt
 pipenv install -r requirements.txt || exit 1
 
-# activate shell of virtual environment
+# Installa le dipendenze di sistema (non Python) per WeasyPrint
+sudo apt update || exit 1
+sudo apt install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info fonts-liberation || exit 1
+
+
+# Attiva l'ambiente virtuale
 exec pipenv shell
